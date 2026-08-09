466db3ada02f5a5f557409ff81562ed2a5b5be2f
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-08-09 20:36:01 +0200

    Add default argument to welcome function

--- Lec_Codes /02.code.cpp
+++ Lec_Codes /02.code.cpp
@@ -1,1 +1,17 @@
 
+#include <iostream>
+using namespace std;
+
+/* 1. Default Args*/
+void welcome( string name="Student name ") {
+    cout << "Hello, " << name << endl; 
+}
+
+int main()
+{
+    welcome("Esraa");
+    welcome();
+    return 0;
+}
+
+
