aa48198d6350296edb79c2473bde0a18bf691316
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-08-10 18:29:57 +0200

    Implement Complex class with friend function for addition

--- Lec_Codes /03.code.cpp
+++ Lec_Codes /03.code.cpp
@@ -1,1 +1,49 @@
-0
+
+// Friend function :
+class Complex
+{
+private:
+    float real;
+    float imag;
+
+public:
+    Complex(float r, float i)
+    {
+        real = r;
+        imag = i;
+    }
+
+    void print()
+    {
+        cout << real << " + " << imag << "i" << endl;
+    }
+
+     friend Complex addTo(float v, Complex c);
+};
+
+Complex addTo(float v, Complex c)
+{
+    c.real = c.real + v;
+
+    return c;
+}
+
+int main()
+{
+    Complex c(2, 5);
+
+    Complex result = addTo(4, c);
+
+    result.print(); 
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
