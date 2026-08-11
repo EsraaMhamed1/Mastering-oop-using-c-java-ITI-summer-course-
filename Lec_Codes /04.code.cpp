471bc4cf00683fd1153c62d930a57ef29b03bee4
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-08-11 23:00:13 +0200

    Add Stack class with push and assignment operator

--- Lec_Codes /04.code.cpp
+++ Lec_Codes /04.code.cpp
@@ -104,8 +104,118 @@ int main()
 
     return 0;
 }
-
 /*************************/
+class Stack
+{
+private:
+    int* st;
+    int size;
+    int tos;
+
+public:
+
+    Stack(int s)
+    {
+        size = s;
+        tos = 0;
+        st = new int[size];
+    }
+
+    void push(int value)
+    {
+        if (tos < size)
+        {
+            st[tos] = value;
+            tos++;
+        }
+    }
+
+    
+    void print()
+    {
+        for (int i = 0; i < tos; i++)
+        {
+            cout << st[i] << " ";
+        }
+
+        cout << endl;
+    }
+
+    Stack& operator=(const Stack& s)
+    {
+        delete[] this->st;
+
+        this->size = s.size;
+        this->tos = s.tos;
+
+        this->st = new int[this->size];
+
+        for (int i = 0; i < this->tos; i++)
+        {
+            this->st[i] = s.st[i];
+        }
+        return *this;
+    }
+
+    ~Stack()
+    {
+        delete[] st;
+    }
+};
+
+
+int main()
+{
+    Stack s1(3);
+
+    s1.push(10);
+    s1.push(20);
+    s1.push(30);
+
+
+    Stack s2(2);
+
+    s2.push(100);
+    s2.push(200);
+
+
+    Stack s3(5);
+
+    s3.push(500);
+
+
+    cout << "Before assignment:" << endl;
+
+    cout << "s1: ";
+    s1.print();
+
+    cout << "s2: ";
+    s2.print();
+
+    cout << "s3: ";
+    s3.print();
+
+
+    // Chained assignment
+    s3 = s2 = s1;
+
+
+    cout << "\nAfter assignment:" << endl;
+
+    cout << "s1: ";
+    s1.print();
+
+    cout << "s2: ";
+    s2.print();
+
+    cout << "s3: ";
+    s3.print();
+
+
+    return 0;
+}
+
+// complex operator oveload 
 
 
 
