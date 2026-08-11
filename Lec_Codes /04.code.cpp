56d8cb6e3e5e78fcb1b3dc02d2c2c6945146bac8
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-08-11 21:59:27 +0200

    Add Stack class with push and assignment operator

--- Lec_Codes /04.code.cpp
+++ Lec_Codes /04.code.cpp
@@ -18,5 +18,95 @@ int main()
     cout << "y : "<<y << endl;
 }
 
-// objects 
+/* objects */
+class Stack
+{
+private:
+    int* st;
+    int size;
+    int tos;
+
+public:
+
+    Stack(int s)
+    {
+        size = s;
+        tos = 0;
+        st = new int[size];
+    }
+
+    void push(int value)
+    {
+        if (tos < size)
+        {
+            st[tos] = value;
+            tos++;
+        }
+    }
+
+
+    void print()
+    {
+        for (int i = 0; i < tos; i++)
+        {
+            cout << st[i] << " ";
+        }
+
+        cout << endl;
+    }
+
+    void operator=(Stack s)
+    {
+        delete[] this->st;
+
+        this->tos = s.tos;
+        this->size = s.size;
+
+        this->st = new int[size];
+
+        for (int i = 0; i < tos; i++)
+        {
+            this->st[i] = s.st[i];
+        }
+    }
+};
+
+int main()
+{
+    Stack s1(3);
+
+    s1.push(10);
+    s1.push(20);
+    s1.push(30);
+
+    Stack s2(2);
+
+    s2.push(100);
+    s2.push(200);
+
+    cout << "Before assignment:" << endl;
+
+    cout << "s1: ";
+    s1.print();
+
+    cout << "s2: ";
+    s2.print();
+
+    s2 = s1;
+
+    cout << "\nAfter assignment:" << endl;
+
+    cout << "s1: ";
+    s1.print();
+
+    cout << "s2: ";
+    s2.print();
+
+    return 0;
+}
+
+/*************************/
+
+
+
 
