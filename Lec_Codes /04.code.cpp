a5dc77a232dfdb0a27891401ab6906e3d055cf16
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-08-11 19:31:45 +0200

    Add copy by value example in 04.code.cpp

--- Lec_Codes /04.code.cpp
+++ Lec_Codes /04.code.cpp
@@ -1,1 +1,22 @@
+#includ <iostream> ;
+using namespace std;
+
+// copy by value (attributes ) 
+int main()
+{
+    int x = 5;
+    int y = 7;
+
+    y = x;
+
+    cout <<"x : "<< x << endl;
+    cout << "y : "<<y << endl;
+
+    x = 3;
+
+    cout <<"x : "<< x << endl;
+    cout << "y : "<<y << endl;
+}
+
+// objects 
 
