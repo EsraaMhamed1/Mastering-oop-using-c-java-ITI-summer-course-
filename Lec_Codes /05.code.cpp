bf558c2ab9c7392de3cd2f82d523bfa3dd7a44fa
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-08-12 21:04:25 +0200

    Implement Complex class with static memory allocation

--- Lec_Codes /05.code.cpp
+++ Lec_Codes /05.code.cpp
@@ -1,1 +1,53 @@
+#include <iostream> 
+using namespace std; 
+
+// static moemory allocation 
+class Complex
+{
+private:
+    double real;
+    double imag;
+
+public:
+    Complex()
+    {
+        real = 0;
+        imag = 0;
+    }
+
+    Complex(double r)
+    {
+        real = r;
+        imag = 0;
+    }
+
+    Complex(double r, double i)
+    {
+        real = r;
+        imag = i;
+    }
+
+    void printComplex()
+    {
+        cout << real << " + " << imag << "i" << endl;
+    }
+};
+
+int main()
+{
+    Complex arr[3] = {
+        Complex(2),
+        Complex(),
+        Complex(5, 7)
+    };
+
+    for (int i = 0; i < 3; i++)
+    {
+        arr[i].printComplex();
+    }
+
+    return 0;
+}
+
+
 
