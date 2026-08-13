9bea626b19a21f1b1071295557873835387e0813
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-08-13 21:08:12 +0200

    Implement Animal and Dog classes with methods
    Added classes for Animal and Dog with basic methods.

--- Lec_Codes /06.code.cpp
+++ Lec_Codes /06.code.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+using namespace std; 
+
+
+// inheritance 
+class Animal {
+public:
+	void eat() {
+		cout << "Eat" << endl; 
+
+	};
+	void sleep() {
+		cout << "Sleep " << endl;
+	};
+};
+
+class Dog : public Animal {
+public:
+	void bark() {
+		cout << "Bark" << endl;
+	};
+};
+
+
+int main() {
+	Dog d; 
+
+	cout << "dog : " << endl;
+	 d.bark();
+	 d.sleep();
+	 d.eat();
+	
+	return 0; 
+}
 
