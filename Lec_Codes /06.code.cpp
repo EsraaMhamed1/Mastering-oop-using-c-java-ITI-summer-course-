ece92554e489aebd9be8ebaf8bfba34d1d09413c
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-08-13 22:09:21 +0200

    Implement Base and Derived classes with product methods
    Added Base and Derived classes with constructors and methods for handling integer values and calculating products.

--- Lec_Codes /06.code.cpp
+++ Lec_Codes /06.code.cpp
@@ -73,3 +73,133 @@ int main() {
 
 	return 0;
 }
+
+/**********************************/
+
+class Base
+{
+private:
+    int a;
+    int b;
+
+public:
+    Base()
+    {
+        a = 0;
+        b = 0;
+        cout << "Base Default Constructor" << endl;
+    }
+
+    Base(int n)
+    {
+        a = n;
+        b = n;
+        cout << "Base(int) Constructor" << endl;
+    }
+
+    Base(int x, int y)
+    {
+        a = x;
+        b = y;
+        cout << "Base(int, int) Constructor" << endl;
+    }
+
+    void setA(int x)
+    {
+        a = x;
+    }
+
+    void setB(int y)
+    {
+        b = y;
+    }
+
+    int getA()
+    {
+        return a;
+    }
+
+    int getB()
+    {
+        return b;
+    }
+
+    int productAB()
+    {
+        return a * b;
+    }
+};
+
+class Derived : public Base
+{
+private:
+    int c;
+
+public:
+    Derived() : Base()
+    {
+        c = 0;
+        cout << "Derived Default Constructor" << endl;
+    }
+
+    Derived(int n) : Base(n)
+    {
+        c = n;
+        cout << "Derived(int) Constructor" << endl;
+    }
+
+    Derived(int x, int y, int z) : Base(x, y)
+    {
+        c = z;
+        cout << "Derived(int, int, int) Constructor" << endl;
+    }
+
+    void setC(int z)
+    {
+        c = z;
+    }
+
+    int getC()
+    {
+        return c;
+    }
+
+    int productABC()
+    {
+        return productAB() * c;
+    }
+};
+
+int main()
+{
+    cout << " Object 1" << endl;
+    Derived d1;
+
+    cout << "A = " << d1.getA() << endl;
+    cout << "B = " << d1.getB() << endl;
+    cout << "C = " << d1.getC() << endl;
+    cout << "Product ABC = " << d1.productABC() << endl;
+
+
+    cout << "\n Object 2 " << endl;
+
+    Derived d2(5);
+
+    cout << "A = " << d2.getA() << endl;
+    cout << "B = " << d2.getB() << endl;
+    cout << "C = " << d2.getC() << endl;
+    cout << "Product ABC = " << d2.productABC() << endl;
+
+
+    cout << "\n Object 3 " << endl;
+
+    Derived d3(2, 3, 4);
+
+    cout << "A = " << d3.getA() << endl;
+    cout << "B = " << d3.getB() << endl;
+    cout << "C = " << d3.getC() << endl;
+    cout << "Product ABC = " << d3.productABC() << endl;
+
+
+    return 0;
+}
