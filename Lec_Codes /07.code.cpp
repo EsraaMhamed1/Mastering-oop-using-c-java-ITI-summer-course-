23d1a8ca293179b4beeddd316b53b3f83d9e65bd
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-08-16 21:12:35 +0200

    Implement protected access in Base and Derived classes
    Added a demonstration of protected access specifier in C++.

--- Lec_Codes /07.code.cpp
+++ Lec_Codes /07.code.cpp
@@ -47,3 +47,45 @@ int main()
 }
 
 
+//2. Protected :
+
+class Base
+{
+public:
+    int a;
+
+protected:
+    int b;
+
+private:
+    int c;
+};
+
+class Derived : protected Base
+{
+public:
+    void show()
+    {
+        a = 10;   // Allowed
+        b = 20;   // Allowed
+        // c = 30;   // NOT Allowed
+
+        cout << "a = " << a << endl;
+        cout << "b = " << b << endl;
+    }
+};
+
+int main()
+{
+    Derived obj;
+
+    // obj.a = 100;  // NOT Allowed
+    // obj.b = 200;  // NOT Allowed
+    // obj.c = 300;  // NOT Allowed
+
+    obj.show();
+
+    return 0;
+}
+
+
