bd8f70065dcad2e0be7dbd56cbeb02a1ae95f52f
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-08-16 22:51:23 +0200

    Add geometric shape classes with area calculations

--- Lec_Codes /07.code.cpp
+++ Lec_Codes /07.code.cpp
@@ -133,5 +133,164 @@ int main()
 
 
 
+//Example 
+class GeoShape
+{
+protected:
+    float dim1;
+    float dim2;
+
+public:
+
+    GeoShape()
+    {
+        dim1 = dim2 = 0;
+    }
+
+    GeoShape(float x)
+    {
+        dim1 = dim2 = x;
+    }
+
+    GeoShape(float x, float y)
+    {
+        dim1 = x;
+        dim2 = y;
+    }
+
+    void setDim1(float x)
+    {
+        dim1 = x;
+    }
+
+    void setDim2(float x)
+    {
+        dim2 = x;
+    }
+
+    float getDim1()
+    {
+        return dim1;
+    }
+
+    float getDim2()
+    {
+        return dim2;
+    }
+
+    float calculateArea()
+    {
+        return 0;
+    }
+};
+
+
+// ================= Triangle =================
+
+class Triangle : public GeoShape
+{
+public:
+
+    Triangle(float b, float h) : GeoShape(b, h)
+    {
+    }
+
+    float calculateArea()
+    {
+        return 0.5 * dim1 * dim2;
+    }
+};
+
+
+// ================= Rectangle =================
+
+class Rect : public GeoShape
+{
+public:
+
+    Rect(float x, float y) : GeoShape(x, y)
+    {
+    }
+
+    float calculateArea()
+    {
+        return dim1 * dim2;
+    }
+};
+
+// ================= Circle =================
+class Circle : private GeoShape
+{
+public:
+
+    Circle(float r) : GeoShape(r)
+    {
+    }
+
+    void setRadius(float r)
+    {
+        dim1 = dim2 = r;
+    }
+
+    float getRadius()
+    {
+        return dim1;
+    }
+
+    float calculateArea()
+    {
+        return 22.0 / 7 * dim1 * dim2;
+    }
+};
+// ================= Square =================
+
+class Square : private Rect
+{
+public:
+
+    Square(float side) : Rect(side, side)
+    {
+    }
+
+    void setSquareDim(float x)
+    {
+        dim1 = dim2 = x;
+    }
+
+    float getSquareDim()
+    {
+        return dim1;
+    }
+
+    float calculateArea()
+    {
+        return Rect::calculateArea();
+    }
+};
+
+
+int main()
+{
+    Triangle t(10, 20);
+    Rect r(10, 20);
+    Circle c(5);
+    Square s(5);
+
+    cout << "Triangle Area = "
+        << t.calculateArea() << endl;
+
+    cout << "Rectangle Area = "
+        << r.calculateArea() << endl;
+
+    cout << "Circle Area = "
+        << c.calculateArea() << endl;
+
+    cout << "Square Area = "
+        << s.calculateArea() << endl;
+
+    return 0;
+}
+
+
 
 
