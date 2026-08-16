e9fd5cd285f879e43f0451f0543917c64e89a528
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-08-16 21:06:28 +0200

    Update 07.code.cpp

--- Lec_Codes /07.code.cpp
+++ Lec_Codes /07.code.cpp
@@ -1,1 +1,49 @@
+#include <iostream>
+using namespace std; 
+
+// inheritanc types 
+// 1. public : 
+
+class Base
+{
+public:
+    int a;
+
+protected:
+    int b;
+
+private:
+    int c;
+};
+
+class Derived : public Base
+{
+public:
+    void show()
+    {
+        a = 10;   // Allowed
+        b = 20;   // Allowed
+        // c = 30;   // NOT Allowed
+
+        cout << "a = " << a << endl;
+        cout << "b = " << b << endl;
+    }
+};
+
+int main()
+{
+    Derived obj;
+
+    obj.a = 100;   // Allowed because a is public
+    cout << obj.a << endl; 
+
+
+    // obj.b = 200;  // NOT Allowed because b is protected
+    // obj.c = 300;  // NOT Allowed because c is private
+
+    obj.show();
+
+    return 0;
+}
+
 
