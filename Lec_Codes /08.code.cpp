b7cf41710cfeb14333f1d0b772e6fc15340caafa
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-08-17 19:24:05 +0200

    Update 08.code.cpp

--- Lec_Codes /08.code.cpp
+++ Lec_Codes /08.code.cpp
@@ -1,1 +1,55 @@
+#include <iostream>
+using namespace std; 
 
+
+// clear points : 
+class Base
+{
+public:
+    void m1()
+    {
+        cout << "Base m1()" << endl;
+    }
+};
+
+class Derived : public Base
+{
+public:
+    void m1()
+    {
+        cout << "Derived m1()" << endl;
+    }
+
+    void m2()
+    {
+        cout << "Derived m2()" << endl;
+    }
+};
+
+int main()
+{
+    Derived obj;
+    Derived* ptr;
+
+    obj.m1();            // Derived
+    obj.Base::m1();      // Base
+    obj.m2();            // Derived
+
+    ptr = &obj;
+
+    ptr->m1();           // Derived
+    ptr->Base::m1();     // Base
+    ptr->m2();           // Derived
+
+    cout << "-----------------" << endl;
+
+    Base* ptr2;
+
+    ptr2 = &obj;
+
+    ptr2->m1();          // Base
+
+    // ptr2->m2();       // Compile Error
+
+    return 0;
+}
