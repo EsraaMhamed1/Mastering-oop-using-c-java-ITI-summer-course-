64ed8fa9ce07bf0148dc0e7c5d2fb057b1091dc8
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-08-22 20:50:24 +0200

    Add variable handling and conditional logic
    Added variable declarations and conditional logic to demonstrate variable assignment and output.

--- Lec_Codes /Java/03.code.java
+++ Lec_Codes /Java/03.code.java
@@ -71,6 +71,23 @@ public class lec_3_prep {
         System.out.println(f);
         System.out.println(isStudent);
         System.out.println(grade);
+
+
+         /*************************************
+                        Variables 
+         *************************************/
+        int x;
+        x = 10;
+
+        if (x == 10) {
+            int y = 20;
+
+            System.out.println("x and y: " + x + " " + y);
+
+            x = y * 2;
+        }
+
+        System.out.println("x is " + x);
       
     }
     
