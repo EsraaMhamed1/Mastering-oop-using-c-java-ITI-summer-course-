d1577163c19661e366561d38e34ba5897fda6742
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-08-22 23:34:42 +0200

    Implement 2D array with nested loop for printing
    Added a 2D array and nested loop to print its elements.

--- Lec_Codes /Java/03.code.java
+++ Lec_Codes /Java/03.code.java
@@ -172,9 +172,23 @@ public class lec_3_prep {
         System.out.println(matrix[1][2]);
         System.out.println(matrix[2][1]);
               
-              
+// 2d array with nested loop 
+       int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        for (int i = 0; i < matrix.length; i++) {
+
+            for (int j = 0; j < matrix[i].length; j++) {
+
+                System.out.print(matrix[i][j] + " ");
+            }
+
+            System.out.println();
+        }  
     }
     
 }
 
-/************************************************/
