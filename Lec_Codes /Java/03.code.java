6944075dd76ee3c633c674255a0390a36baf4a38
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-08-22 18:56:21 +0200

    Create lec_3_prep class with primitive data types
    Added a Java class demonstrating primitive data types.

--- Lec_Codes /Java/03.code.java
+++ Lec_Codes /Java/03.code.java
@@ -1,1 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lec2;
 
+/**
+ *
+ * @author HP
+ */
+public class lec_3_prep {
+    public static void main(String args[]){
+
+
+      /**************************************
+                Primitive Data types 
+      **************************************/
+//int
+        int age = 22;
+//double 
+        double price = 99.99;
+//float
+        float temperature = 36.5f;
+//char
+        char grade = 'A';
+//bool
+        boolean isStudent = true;
+
+        System.out.println(age);
+        System.out.println(price);
+        System.out.println(temperature);
+        System.out.println(grade);
+        System.out.println(isStudent);
+      
+    }
+    
+}
+
+/************************************************/
