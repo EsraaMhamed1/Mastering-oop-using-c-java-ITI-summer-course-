d53840023ff6d0367e32280e94e5f09c44c3c391
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-08-23 21:42:02 +0200

    Implement arithmetic and increment/decrement examples
    Added a Java program demonstrating arithmetic operators and increment/decrement operations.

--- Lec_Codes /Java/04.cod.java
+++ Lec_Codes /Java/04.cod.java
@@ -1,1 +1,39 @@
+package lec2;
+
+/**
+ *
+ * @author HP
+ */
+public class lec_3_prep {
+    public static void main(String args[]){
+      /************************************
+                Arthimetic operators 
+      ************************************/
+      int a = 10;
+        int b = 3;
+
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+
+      // increment & decrement 
+        int x = 5;
+        System.out.println("x = " + x);
+
+        x++;
+        System.out.println("After x++ = " + x);
+
+        x--;
+        System.out.println("After x-- = " + x);
+
+      /************************************
+                Arthimetic operators 
+      ************************************/
+      
+ 
+
+    }
+}
 
