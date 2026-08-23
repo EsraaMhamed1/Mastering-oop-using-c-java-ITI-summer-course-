548cdad92ad1c3b1b75660d9b02ba8ab2c29ff69
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-08-23 22:57:07 +0200

    Add conditional statement examples in 04.cod.java
    Added examples of if, if-else, if-else if ladder, nested if, and switch statements.

--- Lec_Codes /Java/04.cod.java
+++ Lec_Codes /Java/04.cod.java
@@ -208,7 +208,88 @@ public class lec_3_prep {
 
         System.out.println(even.check(4));
         System.out.println(even.check(7));
+        
 
+          /************************************
+                      if statment   
+        ************************************/ 
+        // simple if 
+        int x = 5 ; 
+        int y = 10  ;    
+        if(x < y){
+            System.out.println("x is less than y");
+        }
+
+        // if else 
+         if(x < y)
+            {
+                System.out.println("x is less than y");
+            }
+            else
+            {
+                System.out.println("x is either equals y or greater than y");
+            }
+
+        // if else if ladder 
+         int month = 4; // April
+        String season;
+
+        if(month == 12 || month == 1 || month == 2)
+            season = "Winter";
+        else if(month == 3 || month == 4 || month == 5)
+            season = "Spring";
+        else if(month == 6 || month == 7 || month == 8)
+            season = "Summer";
+        else if(month == 9 || month == 10 || month == 11)
+            season = "Autumn";
+        else
+            season = "Unknown Month";
+
+        System.out.println("April is in the " + season + ".");
+
+        // nested if
+        int i = 10;
+
+        if (i == 10)
+        {
+            // First if statement
+            if (i < 15)
+                System.out.println("i is smaller than 15");
+
+            // Nested - if statement
+            // Will only be executed if statement above
+            // it is true
+            if (i < 12)
+                System.out.println("i is smaller than 12 too");
+            else
+                System.out.println("i is greater than 15");
+        }
+
+
+        
+        /************************************
+                      if statment   
+        ************************************/ 
+         int i = 9;
+
+        switch (i)
+        {
+            case 0:
+                System.out.println("i is zero.");
+                break;
+
+            case 1:
+                System.out.println("i is one.");
+                break;
+
+            case 2:
+                System.out.println("i is two.");
+                break;
+
+            default:
+                System.out.println("i is greater than 2.");
+        }
+   
     }
 }
 
