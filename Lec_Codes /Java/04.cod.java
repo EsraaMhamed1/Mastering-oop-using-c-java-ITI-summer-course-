334f101acd7f3a386f1e030a4cde083712bff5e0
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-08-23 22:21:42 +0200

    Add instanceof operator examples in Java

--- Lec_Codes /Java/04.cod.java
+++ Lec_Codes /Java/04.cod.java
@@ -113,6 +113,68 @@ public class lec_3_prep {
                instanceof operators   
         ************************************/ 
 
+          class A {
+            int i, j;
+        }
+
+        class B {
+            int i, j;
+        }
+
+        class C extends A {
+            int k;
+        }
+
+        class D extends A {
+            int k;
+        }
+
+        A a = new A();
+        B b = new B();
+        C c = new C();
+        D d = new D();
+
+        if (a instanceof A)
+            System.out.println("a is instance of A");
+
+        if (b instanceof B)
+            System.out.println("b is instance of B");
+
+        if (c instanceof C)
+            System.out.println("c is instance of C");
+
+        if (c instanceof A)
+            System.out.println("c can be cast to A");
+
+//        if (c instanceof D)
+//            System.out.println("c can be cast to D");
+
+        A ob;
+
+        ob = d;
+
+        System.out.println("ob now refers to d");
+
+        if (ob instanceof D)
+            System.out.println("ob is instance of D");
+        else
+            System.out.println("ob is not instance of D");
+
+        ob = c;
+
+        System.out.println("ob now refers to c");
+
+        if (ob instanceof D)
+            System.out.println("ob can be cast to D");
+        else
+            System.out.println("ob cannot be cast to D");
+
+        if (ob instanceof A)
+            System.out.println("ob is instance of A");
+
+
+
+
     }
 }
 
