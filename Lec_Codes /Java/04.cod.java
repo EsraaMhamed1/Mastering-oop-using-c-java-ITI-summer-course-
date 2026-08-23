888563365e4e4332cf6c7c8ac74b4afc18953ef2
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-08-23 21:47:52 +0200

    Implement assignment operators examples
    Added examples of assignment operators in Java.

--- Lec_Codes /Java/04.cod.java
+++ Lec_Codes /Java/04.cod.java
@@ -35,8 +35,24 @@ public class lec_3_prep {
         System.out.println(x);
 
       /************************************
-                Arthimetic operators 
+                assignment operators  
       ************************************/
+        int x = 10;
+
+        x += 5;
+        System.out.println("x += 5 : " + x);
+
+        x -= 3;
+        System.out.println("x -= 3 : " + x);
+
+        x *= 2;
+        System.out.println("x *= 2 : " + x);
+
+        x /= 4;
+        System.out.println("x /= 4 : " + x);
+
+        x %= 3;
+        System.out.println("x %= 3 : " + x);
       
  
 
