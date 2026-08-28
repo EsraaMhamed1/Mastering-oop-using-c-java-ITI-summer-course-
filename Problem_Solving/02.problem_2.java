b719d91e683662e5e91fb3cf70beded105b9ade5
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-08-28 10:41:15 +0200

    Update 02.problem_2.java

--- Problem_Solving/02.problem_2.java
+++ Problem_Solving/02.problem_2.java
@@ -1,1 +1,5 @@
 
+/*******************************************************************************************************************************************************
+Implement Object-Oriented Inheritance. Create a superclass Shape with length and breadth attributes and an area() method that prints the dimensions. 
+Create a subclass Rectangle that inherits from Shape, forwards values via super(), and overrides area() to print the calculated area (length * breadth).
+********************************************************************************************************************************************************/
