70e3de7f7ed299d428ba607d1ffc53472f00bbd9
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-08-29 19:08:21 +0200

    Create Genriv class for ArrayList generics demo
    Added a Java class demonstrating the use of generics with ArrayList.

--- Lec_Codes /Java/05.code.java
+++ Lec_Codes /Java/05.code.java
@@ -1,1 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package customexceptiondemo;
+
+import java.util.ArrayList;
+/**
+ *
+ * @author HP
+ */
+public class Genriv {
+    public static void  main(String args[]){
+
+         /************************************
+                   Before Generics
+          ************************************/ 
+      
+
+        /************************************
+                  After Generics
+        ************************************/ 
+        ArrayList files = new ArrayList();
+
+        files.add("file.txt");
+        files.add("data.txt");
+
+        String file = (String) files.get(0);
+        System.out.println(file);
+
+        
+       }
+    }
+    
 
