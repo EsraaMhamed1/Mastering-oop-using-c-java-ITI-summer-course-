544d75c67edc001643b998b9d9e56d765648fc30
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-08-29 19:14:24 +0200

    Update 05.code.java

--- Lec_Codes /Java/05.code.java
+++ Lec_Codes /Java/05.code.java
@@ -15,18 +15,29 @@ public class Genriv {
          /************************************
                    Before Generics
           ************************************/ 
-      
-
-        /************************************
-                  After Generics
-        ************************************/ 
-        ArrayList files = new ArrayList();
+         ArrayList files = new ArrayList();
 
         files.add("file.txt");
         files.add("data.txt");
+        files.add(10);
 
         String file = (String) files.get(0);
         System.out.println(file);
+        /************************************
+                  After Generics
+        ************************************/ 
+        
+        ArrayList<String> files = new ArrayList<>();
+
+        files.add("file1.txt");
+        files.add("file2.txt");
+//      files.add(10);
+            
+        String file = (String) files.get(0);
+        System.out.println(file);
+
+        
+       
 
         
        }
