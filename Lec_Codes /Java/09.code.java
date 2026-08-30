ec9411cbafa89982e19960f8cf747d9878327115
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-08-30 17:57:49 +0200

    Update 09.code.java

--- Lec_Codes /Java/09.code.java
+++ Lec_Codes /Java/09.code.java
@@ -1,1 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package customexceptiondemo;
 
+/**
+ *
+ * @author HP
+ */
+
+
+public class ArrList {
+    public static void main(String args[]){
+
+
+      /***************************************
+                   Collection 
+       **************************************/
+        // import java.util.ArrayList;
+        ArrayList<String> students = new ArrayList<>();
+
+        students.add("Ahmed");
+        students.add("Mona");
+        students.add("Ali");
+
+        System.out.println(students);
+
+
+       /***************************************
+                   Collection 
+       **************************************/
+
+      
+    }
+    
+}
