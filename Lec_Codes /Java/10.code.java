679874c243c19d39913b11e67667c4a95bf6d247
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-08-31 19:20:54 +0200

    Add even number filtering using streams
    Implement filtering of even numbers from a list.

--- Lec_Codes /Java/10.code.java
+++ Lec_Codes /Java/10.code.java
@@ -1,1 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lecture_10;
 
+/**
+ *
+ * @author HP
+ */
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]){
+
+    /*********************************
+                filter()
+    *********************************/
+    List<Integer> numbers =
+                List.of(10, 15, 20, 25, 30, 35);
+
+        List<Integer> evenNumbers =
+                numbers.stream()
+                       .filter(number -> number % 2 == 0)
+                       .toList();
+
+        System.out.println(evenNumbers);
+
+
+      
+    /*********************************
+                filter()
+    *********************************/
+
+      
+    }
+    
+}
